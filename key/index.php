516b0b88f106a6d48edd30466705d5fb7f7d9988
gmwnr <50177786+gmwnr@users.noreply.github.com>
2022-07-09 16:44:21 -0300

    Add files via upload

---
+++ key/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>GameOwner.net</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <meta name="robots" content="noindex">
+
+    <link rel="preload" as="style" href="styles/css.css">
+    <link href="styles/css.css" rel="stylesheet">
+
+    <link rel="preload" as="style" href="styles/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+
+    <link rel="preload" as="style" href="styles/krnl-components.css">
+    <link rel="stylesheet" href="styles/components.css">
+</head>
+
+<body>
+<div class="container">
+                <div class="row h-100 justify-content-center align-items-center">
+                    <form action="?" method="POST" style="margin-top: 40%; transform: translateY(-50%);">
+                        <div class="form-group" >
+                            <!--small class="form-text text-muted text-center">Created by <a href="https://nvrx.party">nvrxstd</a> based on Krnl Key System.</small--><small class="form-text text-muted text-center">Please verify to get key!</small>
+                            <center>
+                                <a onclick="this.href='https://link-target.net/23425/network-graphics';" class="btn btn-primary my-2" style="color:white;">Verify Me</a>
+                            </center>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+</body></html>
